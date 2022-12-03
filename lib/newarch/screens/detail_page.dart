c95eb94d8f6ad735898b7548b24f6da3cd9b4872
anjana-krishna-ns <118959494+anjana-krishna-ns@users.noreply.github.com>
2022-12-03 12:22:16 +0530

    Add files via upload

---
+++ lib/newarch/screens/detail_page.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class DetailPage extends StatefulWidget {
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Stack(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            height: size.height * 0.5,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/images/images/shoe1.png"),
+                  fit: BoxFit.cover),
+            ),
+            child: SafeArea(
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+            Container(
+                child: TextButton(
+                  onPressed: () {
+              Navigator.pop(context);
+              },
+                  child:  Icon(Icons.arrow_back,color: Colors.black,))),
+                    Row(
+                      children: <Widget>[
+                        SvgPicture.asset("assets/images/icons/svg/heart_icon.svg"),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        SvgPicture.asset("assets/images/icons/svg/share_icon.svg"),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: size.height * 0.45),
+            width: double.infinity,
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(50)),
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+
+                  Align(
+                    child: Container(
+                      width: 150,
+                      height: 7,
+                      decoration: BoxDecoration(
+                          color: Colors.red[50],
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        width: 40,
+                        height: 40,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                    "https://images.unsplash.com/photo-1521572267360-ee0c2909d518?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80"),
+                                fit: BoxFit.cover)),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      // Column(
+                      //   crossAxisAlignment: CrossAxisAlignment.start,
+                      //   children: <Widget>[
+                      //     Text(
+                      //       "Jean-Luis",
+                      //       style: TextStyle(
+                      //           fontSize: 16, fontWeight: FontWeight.bold),
+                      //     ),
+                      //     SizedBox(
+                      //       height: 3,
+                      //     ),
+                      //     Text(
+                      //       "Interior Design",
+                      //       style: TextStyle(fontSize: 13),
+                      //     )
+                      //   ],
+                      // )
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    children: <Widget>[
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(3)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Text("convers"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(3)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(6.0),
+                          child: Text("Black"),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 20,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Text("Gallery",style: TextStyle(
+                      fontSize: 18
+                  ),),
+                  SizedBox(height: 20,),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(image: AssetImage("assets/images/images/shoe1.png"),fit: BoxFit.cover)
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(image: AssetImage("assets/images/images/shoe3.png"),fit: BoxFit.cover)
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20),
+                          child: Container(
+                            width: 150,
+                            height: 150,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                image: DecorationImage(image: AssetImage("assets/images/images/shoe4.png"),fit: BoxFit.cover)
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
